1779e1341bc5810e619c44a0fad4bc02d6907793
Daniel Moniz <daniel.moniz@gmail.com>
2018-05-22 13:31:53 -0400

    Add sandbox file for programming fundamentals.

---
+++ 06-programming-fundamentals-part-1/instructor/test.rb
@@ -0,0 +1,152 @@
+
+# puts 3 + 3
+# puts 5 * 9
+# puts 1 / 2.0
+# puts 10 - 4
+# puts 3 ** 3
+#
+# puts 7 % 3
+#
+# # add the number of apples and oranges
+# puts 3 + 3 * 2
+
+# apples_i_own = 5
+#
+#
+#
+# # puts apples_i_own
+#
+# # apples_i_own = apples_i_own + 1
+# apples_i_own += 3
+# puts apples_i_own
+#
+# number_of_fruit = 3 + 5 + 2
+# puts number_of_fruit
+#
+# number_of_vegetables = 10 * 334
+#
+# food_for_feast = number_of_fruit + number_of_vegetables
+# puts food_for_feast
+#
+#
+#
+# message = 'add the number of apples and oranges'
+# puts message
+#
+# signed = ' - Daniel'
+# full_message = message + signed
+# puts full_message
+#
+# name = 'Daniel'
+# full_message = message + ' - ' + name + 3000.to_s
+# puts full_message
+#
+# full_message = "#{message} - #{name}#{3 * 1000}"
+# # full_message = "message - name"
+# puts full_message
+#
+# number_message = "First number: #{3 * 4}, second number is {5 - 2}"
+# puts number_message
+#
+# poem = "The bleak dark sky\ncovering like a blanket"
+# puts poem
+#
+# hello = "She's back!"
+# goodbye = 'She said, "Hello to everyone!"'
+# word_up = "She said, \"Hello to everyone!\""
+# puts word_up
+#
+#
+# puts hello.upcase
+# puts hello.length
+#
+# thoughts = nil
+# puts thoughts
+#
+#
+# puts(puts 6 + 8)
+
+
+
+#
+# i_am_teaching = true
+# sky_is_purple = false
+#
+# apples = 3
+# oranges = 5
+# result = 5 > 3
+# result = apples >= oranges
+# puts result
+
+
+# goats = 5
+
+# if true && true
+#   # code in here will run conditionally
+#   puts "We got in!"
+# end
+
+# sky = "rainy"
+# temperature = -20
+#
+# if sky == 'sunny' || temperature >= 20
+#   puts "Put on shorts and tshirt!"
+# end
+#
+# if sky == 'rainy'
+#   puts "Stay inside."
+#   if temperature < -15
+#     puts 'Turn up the heat.'
+#   end
+# elsif sky == 'purple'
+#   puts "Get to the storm shelter."
+# elsif sky == 'rainy'
+#   puts 'Call my mom.'
+# else
+#   puts "Go out!"
+# end
+
+
+# my_name = gets.chomp
+# puts my_name + " is so cool"
+# puts my_name.inspect
+#
+# print "hello"
+# print "my name is"
+
+
+
+while true
+  user_input = gets.chomp
+  if user_input == 'exit'
+    break
+  end
+
+  puts "The game goes on: #{user_input}"
+end
+
+puts "The game is over!"
+
+
+counter = 0
+while counter < 10
+  puts "Hello world"
+  counter += 1
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
