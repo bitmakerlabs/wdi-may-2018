dc150c33622f87451e730dce8c9bc992dfe4316a
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-06-12 11:41:37 -0400

    [21-intro-to-mvc] Use RESTful routes

--- 21-intro-to-mvc/instructor/crm/config/routes.rb
+++ 21-intro-to-mvc/instructor/crm/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-  get 'contacts/show'
-  get 'contacts/new'
-  get 'contacts/create'
+  # get 'contacts/index'
+  # get 'contacts/show'
+  # get 'contacts/new'
+  # get 'contacts/create'
+
+  resources :contacts
+
+  root 'contacts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
