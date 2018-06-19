1356e32d27a3a98f1d5d713485a548e91e5e34d0
NatalieBlack <natalievictoriablack@gmail.com>
2018-06-19 11:40:46 -0400

    In-class code for second half of 26-associations

--- 26-associations/photogur/app/models/comment.rb
+++ 26-associations/photogur/app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   # there is a picture_id foreign key in your db table
   belongs_to :picture
+  has_many :tags, through: :picture
 end
--- 26-associations/photogur/app/models/picture.rb
+++ 26-associations/photogur/app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ActiveRecord::Base
   # there is a foreign key about you (picture) in the comments table
   has_many :comments
+  has_and_belongs_to_many :tags
 end
