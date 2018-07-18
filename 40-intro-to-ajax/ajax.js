d1b9d794647d78abf811f744f9f8e5372e75298a
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-07-18 11:44:52 -0400

    gurjant push from ajax

--- 40-intro-to-ajax/ajax.js
+++ 40-intro-to-ajax/ajax.js
@@ -1,3 +1,90 @@
-$(function() {
+window.addEventListener('DOMContentLoaded',function(){
+
+console.log("Starting our ajax js")
+
+// get the button
+var getMonsters = document.getElementById('getMonsters');
+var monstersDivTag = document.getElementById('monsters');
+var postMonsters = document.getElementById('postMonsters');
+
+// add event listener
+getMonsters.addEventListener('click',function(){
+
+
+
+//make ajax
+var request = $.ajax({
+	method: 'GET',
+	url: 'https://monsters-api.herokuapp.com/monsters.json',
+	dataType: 'json'
+});
+
+request.done(function(data){
+	// monsters.innerHTML = data;
+
+
+	for (var i = 0; i < data.length ; i++) {
+		monstersDivTag.innerHTML += `<p>` + data[i].name+ `<img src="https://robohash.org/`+data[i].id+`"/></p><br/>` 
+   }
+
+
+	// for (var i = 0; i < data.length ; i++) {
+	// 	var pTag = document.createElement('p');
+	// 	pTag.innerText = data[i].name
+	// 	monsters.append(pTag);
+	// }
+
+});
+
+request.fail(function(a, s, e ){
+	console.log("Oh man, request fails")
+	monsters.innerHTML = e;
+});
+
+request.always(function(){
+	console.log("I will be always here... boooo!!")
+})
+
+
+});
+
+
+
+// Post request with ajax
+postMonsters.addEventListener('click', function(){
+
+var name = $('#name').val()
+var home = $('#home').val()
+var creepiness = $('#creepiness').val()
+
+$.ajax({
+	url: 'https://monsters-api.herokuapp.com/monsters',
+	method: 'POST',
+	data: {
+		monster: {
+			name: name,
+			home: home,
+			creepiness: creepiness
+		}
+	}
+}).done(function(data){
+	console.log('Every went fine');
+	console.log(data)
+monstersDivTag.innerHTML += `<p>` + data.name+ `<img src="https://robohash.org/`+data.id+`?set=set2
+"/></p><br/>` 
+
+
+}).fail(function(){
+	console.log("Something went wrong")
+})
+
+});
+
+
+
+
+
+console.log("Ending our ajax js")
+
 
 });
--- 41-ajax-and-json/instructor/robots/app/assets/javascripts/robots.js
+++ 41-ajax-and-json/instructor/robots/app/assets/javascripts/robots.js
@@ -3,4 +3,80 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+console.log("Starting robots js")
+
+
+//Select All links
+// var roboLinks = document.querySelectorAll('')
+var roboContainer = document.getElementById('robot-container')
+var roboDetails = document.getElementById('robot-details-container')
+
+
+
+// Add Event to links
+// for (var i = 0; i < roboLinks.length; i++) {
+
+// 		roboLinks[i].addEventListener('click',function(e){
+
+// 			// Stop a from going to another page
+// 			e.preventDefault();
+
+// 			// get the link
+// 			console.log(this.href)
+
+// 		})
+
+
+
+// }
+
+roboContainer.addEventListener('click', function(e){
+	//Stop a from going to another page
+			e.preventDefault();
+			if (e.target.className != 'robot-box') {
+
+				var url = e.target.parentNode.href;
+
+
+				// making ajax call
+				$.ajax({
+					url: url,
+					method: 'GET',
+					dataType: 'json'
+				}).done(function(data){
+					console.log(data)
+					// var divTag = document.createElement('div');
+					// roboDetails.innerHTML = ""
+					// var imgTag = document.createElement('img');
+					// imgTag.src = "https://robohash.org/" + data.address
+
+					// var pTag = document.createElement('p');
+					// pTag.innerText = data.name
+
+					// roboDetails.append(imgTag);
+					// roboDetails.append(pTag);
+
+					var source   = document.getElementById("entry-template").innerHTML;
+					var template = Handlebars.compile(source);
+					 roboDetails.innerHTML = template(data)
+
+					// roboDetails.innerHTML = `<img src="https://robohash.org/`+data.address+`"/><p>`+data.name+`</p>`;
+				})
+
+
+
+
+			}
+
+})
+
+
+
+
+
+console.log("Ending robots js")
+
+
+
+
 });
